38f17e7d41a18e9c47569cd319b86a563299136c
Prachi <prachikale136@gmail.com>
2023-02-05 14:20:21 +0530

    compute third Equation

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -5,6 +5,7 @@ Echo "Arithmatic And Computaion Sorting"
 read -p "Enter the 1st value: " a
 read -p "Enter the 2nd value: " b
 read -p "Enter the 3rd value: " c
-result=$((($a*$b)+$c))
-echo "Result of (a*b+c)=" $result
+result=$((($c+$a)/$b))
+
+echo "Result of (c+a/b)=" $result
 

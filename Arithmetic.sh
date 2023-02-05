3d60a3b0344fcc23d8c6eaeabfdfb68722e502ce
Prachi <prachikale136@gmail.com>
2023-02-05 14:03:33 +0530

    code for taking input

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -1,2 +1,11 @@
 Echo "Arithmatic And Computaion Sorting"
 
+#!bin/bash -x
+
+read -p "Enter the 1st value: " a
+read -p "Enter the 2nd value: " b
+read -p "Enter the 3rd value: " c
+echo "a="$a;
+echo "b="$b;
+echo "c="$c;
+
